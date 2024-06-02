a9aaf40d78524fcbf49e10a95655a66889d27599
fahminurcahya <fahminurcahya@gmail.com>
2024-06-02 18:31:42 +0700

    init laravel & breeze, spetie, migrate table

---
+++ database/migrations/2024_06_02_112300_create_bookings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('field_id')->constrained('fields')->onDelete('cascade');
+            $table->foreignId('time_slot_id')->constrained('time_slots')->onDelete('cascade');
+            $table->decimal('total_amount', 10, 2);
+            $table->text('description')->nullable();
+            $table->enum('payment_status', ['DP', 'Lunas']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
